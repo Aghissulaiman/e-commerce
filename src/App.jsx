bf9b9ea9c7c6c792849ad7598f7e37882769a0d4
Aghissulaiman <aghis97531>
2025-11-07 20:32:38 +0700

    updete 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Landingpages from './pages/Landingpages'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Landingpages from './pages/Landingpages'
 import Loginpage from './pages/Loginpage'
 import Daftar from './components/Daftar'
 import Homepages from './pages/Homepages'
@@ -11,23 +8,24 @@ import Checkout from './components/Checkout'
 import KeranjangPege from './pages/KeranjangPege'
 import ProfilPage from './pages/ProfilPage'
 import Daftarpages from './pages/Daftarpages'
-import Historipage from './pages/historipage'
+import Historipage from './pages/Historipage' 
+
 function App() {
   return (
     <>
-<BrowserRouter>
-  <Routes>
-    <Route index element={<Landingpages />} />
-    <Route path="login" element={<Loginpage />} />
-    <Route path="register" element={<Daftar />} />
-    <Route path="Home" element={<Homepages />} />
-    <Route path="Detail/:id" element={<Detail />} />
-    <Route path="Checkout" element={<Checkout/>} />
-    <Route path="Keranjang" element={<KeranjangPege/>} />
-    <Route path="profil" element={<ProfilPage />} />
-    <Route path="Histori" element={<Historipage />} />
-  </Routes>
-</BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Landingpages />} />
+          <Route path="login" element={<Loginpage />} />
+          <Route path="register" element={<Daftar />} />
+          <Route path="Home" element={<Homepages />} />
+          <Route path="Detail/:id" element={<Detail />} />
+          <Route path="Checkout" element={<Checkout />} />
+          <Route path="Keranjang" element={<KeranjangPege />} />
+          <Route path="profil" element={<ProfilPage />} />
+          <Route path="Histori" element={<Historipage />} /> {/* ✅ Sudah benar */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
